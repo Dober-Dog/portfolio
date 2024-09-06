c2d5f88c3910767dcb4c9f4ffc905e0e2ccc7134
Logan Russell <Logan1135@gmail.com>
2024-09-06 08:51:50 -0600

    attempting to fix image issue

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,10 +4,10 @@ export default function Card (props){
         <div className='card'>
             <h1>{props.websiteName}</h1>
             <div className='card-links'>
-                <a href={props.webLink} target='blank'>Visit Site</a>
-                <a href={props.gitLink} target='blank'>View Code</a>
+                <a href={props.webLink} target='_blank' rel='noopener noreferrer'>Visit Site</a>
+                <a href={props.gitLink} target='_blank' rel='noopener noreferrer'>View Code</a>
             </div>
-            <img src={props.img} alt={`${props.websiteName} image`} height="400px"/>
+            <img src={`${process.env.PUBLIC_URL}${props.img}`} alt={`${props.websiteName} image`} height="400px"/>
             <hr></hr>
         </div>
     )
--- src/components/links.js
+++ src/components/links.js
@@ -3,13 +3,13 @@ const links = [
         websiteName: "DoberDog's Meme Generator",
         webLink: " https://doberdogmemes.netlify.app/",
         gitLink: "https://github.com/Dober-Dog/meme-generator",
-        img: "src/components/Screenshot 2024-09-01 214628.png"
+        img: "public/Screenshot 2024-09-01 214628.png"
     },
     {
         websiteName: "DoggyFacts",
         webLink: "https://doggyfacts.netlify.app/",
         gitLink: "https://github.com/Dober-Dog/dog-facts",
-        img: "src/components/Screenshot 2024-09-01 215310.png"
+        img: "public/Screenshot 2024-09-01 215310.png"
     }
 ]
 
